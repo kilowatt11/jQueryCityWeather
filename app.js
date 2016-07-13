7b79eabdd1b6e51f8eeae8c6f2be578b7c120459
Kurt Hammes <kilowatt11@cableone.net>
2016-07-13 14:46:27 -0600

    fixed to use promise. Added inout field

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 (function () {
 
 		$('#newZipCode').on('keypress',function(event){
-			console.log(event);
+			
 			if(event.keyCode === 13){
 				event.preventDefault();
 			}
-		})
+		});
+
 	$('#addCityBtn').on('click', function () {
+		var cityName = $('#newCity').val();
+		var state = $('#newState').val();
+		var cityData = null;
 
-		var zip = $('#newZipCode').val();
 		
-
-		// get the city & weather data
-		// add a city panel to the list	
-
-		getCity(zip, function (cityData) {
-			console.log(cityData);
-			getWeather(zip, function (weatherData) {
-				console.log(weatherData)
+		$.when(getCity(state, cityName))
+		.then(function(cityinfo){
+			cityData = cityinfo
+			return $.when(getWeather(cityinfo))
+		})
+		.then(function (weather){
 				var template = $('#cityTemplate').text()
 					.replace('{{ city }}', cityData.city)
 					.replace('{{ state }}', cityData.state)
-					.replace('{{ temp }}', weatherData.temp)
-					.replace('{{ conditions }}', weatherData.conditions);
+					.replace('{{ temp }}', weather.temp)
+					.replace('{{ conditions }}', weather.conditions);
 				$('#cityList').append($(template));
-				$('#newZipCode').val('');
+				$('#newCity').val('');
+				$('#newState').val('');
+		})
+			
 
-			})
-		});
+			
+		
 		$('#cityList').on('click','.close',function(event){
 			event.target.closest('.city').remove();
 		})
@@ -36,41 +40,41 @@
 	});
 
 
-	function getCity(zip, callback) {
-
-
+	function getCity(state, cityName) {
+		var dfd = jQuery.Deferred();
 		var urlBase = 'http://api.zippopotam.us/us/';
-		var url = urlBase + zip;
+		var url = urlBase + state + '/'+ cityName;
+		console.log(url);
+		
 		$.get(url, function (response) {
-			callback({
+			dfd.resolve({
 				city: response.places[0]['place name'],
-				state: response.places[0]['state abbreviation']
-			})
-
-
-
+				state: response['state abbreviation'],
+				zip: response.places[0]['post code']
+			});
 		});
-
-
-		// use jQuery get to get the zip code data	
-		// transform the data to camelCase proprty names	
-
+		return dfd.promise();
 	}
 
-	function getWeather(zip, callback) {
+	function getWeather(cityinfo) {
+		var dfd = jQuery.Deferred();
 
 		var urlBase = 'http://api.openweathermap.org/data/2.5/';
 		var appId = 'bd82255fd0a21fa1238699b9eda2ee35';
-		var url = urlBase + 'weather?appid=' + appId + '&units=imperial&zip=' + zip;
-		$.get(url, function (response) {
-			callback({
+		var url = urlBase + 'weather?appid=' + appId + '&units=imperial&zip=' + cityinfo.zip;
+
+		$.when($.get(url))
+		.then(function(response){
+console.log(response)
+			dfd.resolve({
+
 				temp: response.main.temp,
 				conditions: response.weather[0].description
-			})
-
-
+			});
 
 		});
+
+		return dfd.promise();
 
 		// use jQuery get to get the zip code weather data
 	}
